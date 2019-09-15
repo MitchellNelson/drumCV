92466c6d388a48e5bf90e686a96dc4dc2618bd3c
Mitchell Nelson <nels4929@stthomas.edu>
2019-09-15 13:21:22 -0500

    Volume Adjust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from collections import deque
-from imutils.video import VideoStream
-from imutils.video import WebcamVideoStream
+from webcamvideostream import WebcamVideoStream
+import os
 import numpy as np
 import argparse
 import cv2
@@ -12,28 +12,41 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
     help="path to the (optional) video file")
 ap.add_argument("-b", "--buffer", type=int, default=8,
-    help="max buffer size")
+   help="max buffer size")
 args = vars(ap.parse_args())
 
-lpts = deque(maxlen=args["buffer"])
-rpts = deque(maxlen=args["buffer"])
+lpts = deque(maxlen=8)#args["buffer"]) #buffer to hold left stick coordinates
+rpts = deque(maxlen=8)#args["buffer"]) #buffer to hold right stick coordinates
 
-xQue = deque(maxlen=2)
-center = deque(maxlen=2)
+xQue = deque(maxlen=2) #length 2 buffer to hold current x-axis positions of l and r sticks
+center = deque(maxlen=2) #length 2 buffer to hold center positions of l and r sticks
 
 (lisDown, risDown) = (False, False)
 
 objLower = (30, 86, 14)
 objUpper = (97, 244, 255)
 
-wave_clap = sa.WaveObject.from_wave_file("audio/Clap.wav")
-wave_kick = sa.WaveObject.from_wave_file("audio/Kick.wav")
-wave_hat = sa.WaveObject.from_wave_file("audio/Hat.wav")
-play_clap = wave_clap.play()
-play_kick = wave_kick.play()
-play_hat = wave_hat.play()
+wave_clap  = sa.WaveObject.from_wave_file("audio/Clap.wav")
+wave_clap2 = sa.WaveObject.from_wave_file("audio/Clap2.wav")
+wave_clap3 = sa.WaveObject.from_wave_file("audio/Clap3.wav")
+wave_kick  = sa.WaveObject.from_wave_file("audio/Kick.wav")
+wave_kick2 = sa.WaveObject.from_wave_file("audio/Kick2.wav")
+wave_kick3 = sa.WaveObject.from_wave_file("audio/Kick3.wav")
+wave_hat   = sa.WaveObject.from_wave_file("audio/Hat.wav")
+wave_hat2  = sa.WaveObject.from_wave_file("audio/Hat2.wav")
+wave_hat3  = sa.WaveObject.from_wave_file("audio/Hat3.wav")
 
-counter = 0
+play_clap  = wave_clap.play()
+play_clap2 = wave_clap2.play()
+play_clap3 = wave_clap3.play()
+play_kick  = wave_kick.play()
+play_kick2 = wave_kick2.play()
+play_kick3 = wave_kick3.play()
+play_hat   = wave_hat.play()
+play_hat2  = wave_hat2.play()
+play_hat3  = wave_hat3.play()
+
+frameCount = 0
 (ldY, rdirY) = (0, 0)
 
 vs = WebcamVideoStream(src=0).start()
@@ -41,19 +54,15 @@ time.sleep(1.0)
 
 while True:
     frame = vs.read()
-    frame = imutils.resize(frame, width=600)
     frame = cv2.flip(frame, 1)
-
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     mask = cv2.inRange(hsv, objLower, objUpper)
     mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
 
     # find contours in the mask and initialize the current
     # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
 
     # sort cnts so we can loop through the two biggest (the sticks hopefully)
@@ -61,8 +70,7 @@ while True:
     for i in range(min(len(cnts), 2)):
         ((x, y), radius) = cv2.minEnclosingCircle(cnts[i])
         xQue.appendleft(x)
-        M = cv2.moments(cnts[i])
-        center.appendleft((int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])))
+        center.appendleft((int(x),int(y)))
     for i in range(min(len(cnts), 2)):
         if (len(cnts) > 1):
             if (xQue[i] <= xQue[(i+1)%2]):
@@ -74,54 +82,64 @@ while True:
                 left = True
             else: 
                 left = False
+
+        #process left stick
         if (left):
             lpts.appendleft(center[i])
-            for j in np.arange(1, 8):
-                if counter >= 8 and j == 1 and lpts[-8] is not None:
-                    # compute the difference between the x and y
-                    # coordinates and re-initialize the direction
-                    ldY = lpts[-8][1] - lpts[j][1]
-                    if(lisDown and ldY < 0):
-                        if (xQue[i] <=200):
-                            play_kick = wave_kick.play()
-                        elif (xQue[i] <=400):
-                            play_clap = wave_clap.play()
-                        else:
-                            play_hat = wave_hat.play()
-                        lisDown = False
-
-                    if np.abs(ldY) > 30 and ldY >=0:
-                        #if(ldY >= 0):
-                        lisDown = True
+            if frameCount >= 4 and lpts[4] is not None:
+                # compute the difference in direction between current frame
+                # and from 4-frames-ago
+                ldY = lpts[4][1] - lpts[1][1]
                 
-            cv2.circle(frame, center[i], 5, (0, 0, 255), -1)
+                #Threshold of 10 pixels for downward direction
+                if(lisDown and ldY < -20):
+                    # Play sound according to x position
+                    # Play different volume sample based on distance moved
+                    if (xQue[i] <=200):
+                        play_kick = wave_kick.play()
+                    elif (xQue[i] <=400):
+                        print(ldY)
+                        if (ldY < -70):
+                            play_clap = wave_clap.play()
+                        elif (ldY < -60):
+                            play_clap2 = wave_clap.play()
+                        else:
+                            play_clap3 = wave_clap3.play()
+                    else:
+                        play_hat = wave_hat.play()
+                    lisDown = False
+
+                if np.abs(ldY) > 30 and ldY >=0:
+                    lisDown = True
+            
+        #process right stick
         else:
             rpts.appendleft(center[i])
-            for j in np.arange(1, 8):
-                if counter >= 8 and j == 1 and rpts[-8] is not None:
-                    # compute the difference between the x and y
-                    # coordinates and re-initialize the direction
-                    rdY = rpts[-8][1] - rpts[j][1]
-                    if(risDown and rdY < 0):
-                        if (xQue[i] <=200):
-                            play_kick = wave_kick.play()
-                        elif (xQue[i] <=400):
-                            play_clap = wave_clap.play()
-                        else:
-                            play_hat = wave_hat.play()
-                        risDown = False
+            if frameCount >= 4 and rpts[4] is not None:
+                # compute the difference in direction between current frame
+                # and from 4-frames-ago
+                rdY = rpts[4][1] - rpts[1][1]
 
-                    if np.abs(rdY) > 30 and rdY >=0:
-                        #if(rdY >= 0):
-                        risDown = True
-                
-            cv2.circle(frame, center[i], 5, (255, 0, 0), -1)
+                #Threshold of 10 pixels for downward direction 
+                if(risDown and rdY < -20):
+                    #Play sound according to x position
+                    if (xQue[i] <=200):
+                        play_kick = wave_kick.play()
+                    elif (xQue[i] <=400):
+                        play_clap = wave_clap.play()
+                    else:
+                        play_hat = wave_hat.play()
+                    risDown = False
 
-    # show the frame to our screen and increment the frame counter
+                if np.abs(rdY) > 30 and rdY >=0:
+                    #if(rdY >= 0):
+                    risDown = True
+            
+    # show the frame to our screen and increment the frame count
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
-    counter += 1
-
+    frameCount += 1
+    
     # if the 'q' key is pressed, stop the loop
     if key == ord("q"):
         break
