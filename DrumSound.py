1db77ecf1e713b08c8f26c0cb68b82a8e351605f
Mitchell Nelson <nels4929@stthomas.edu>
2020-03-25 09:14:01 -0500

    Add 3 sound split

--- DrumSound.py
+++ DrumSound.py
@@ -14,8 +14,14 @@ class DrumSound:
         for i in range(1, self.numberOfSounds + 1):
             path = "" + self.pathToAudioFolder + "/"+ self.name + str(i) + self.audioExtention
             sound = sa.WaveObject.from_wave_file(path)
-            print(i)
             self.sounds.append(sound)
 
     def play(self, volumeIndex):
-        self.sounds[volumeIndex].play()
+        # Play loudest sound if given volume is greater than max 
+        if (volumeIndex > self.numberOfSounds - 1):
+            self.sounds[self.numberOfSounds -1].play()
+        # Play softest sound if less than 0
+        elif (volumeIndex < 0):
+            self.sounds[0].play()
+        else:
+            self.sounds[volumeIndex].play()
